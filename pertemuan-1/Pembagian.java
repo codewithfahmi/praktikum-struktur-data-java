4869115953199ccda6acd9bd3ba06f9df043a4ff
codewithfahmi <fahmiyahya0@gmail.com>
2024-09-13 02:28:53 +0700

    update(praktikum - 1/Pembagian.java): change `double` to `int` at `banyak_apel` variable

--- pertemuan-1/Pembagian.java
+++ pertemuan-1/Pembagian.java
@@ -1,6 +1,6 @@
 class Pembagian {
     public static void main(String[] args) {
-        int banyak_apel  = 5;
+        double banyak_apel  = 5;
         int jumlah_anak  = 2;
         // int perolehan   = banyak_apel / jumlah_anak;
         double perolehan = banyak_apel / jumlah_anak;
