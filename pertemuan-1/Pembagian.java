9cc617f11bc65b3e551e666352c0307b86771163
codewithfahmi <fahmiyahya0@gmail.com>
2024-09-13 01:47:51 +0700

    update(pertemuan-1/Pembagian.java): change `int` to `double` at `perolehan` variable and change String.format() with raw string print

--- pertemuan-1/Pembagian.java
+++ pertemuan-1/Pembagian.java
@@ -1,8 +1,9 @@
 class Pembagian {
     public static void main(String[] args) {
-        int banyak_apel = 5;
-        int jumlah_anak = 2;
-        int perolehan   = banyak_apel / jumlah_anak;
-        System.out.println(String.format("Masing - masing mendapat = %d", perolehan));
+        int banyak_apel  = 5;
+        int jumlah_anak  = 2;
+        // int perolehan   = banyak_apel / jumlah_anak;
+        double perolehan = banyak_apel / jumlah_anak;
+        System.out.println("Masing - masing mendapatkan = " + perolehan);
     }
 }
