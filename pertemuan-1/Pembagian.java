e1a0a811fdea12b3adc4d53f72e475b375c2c256
codewithfahmi <fahmiyahya0@gmail.com>
2024-09-15 15:59:45 +0700

    fix(pertemuan - 1/Pembagian.java): change int to double in `banyak_apel` variable

--- pertemuan-1/Pembagian.java
+++ pertemuan-1/Pembagian.java
@@ -1,8 +1,7 @@
 class Pembagian {
     public static void main(String[] args) {
-        double banyak_apel  = 5;
-        int jumlah_anak  = 2;
-        // int perolehan   = banyak_apel / jumlah_anak;
+        double banyak_apel = 5;
+        int jumlah_anak = 2;
         double perolehan = banyak_apel / jumlah_anak;
         System.out.println("Masing - masing mendapatkan = " + perolehan);
     }
