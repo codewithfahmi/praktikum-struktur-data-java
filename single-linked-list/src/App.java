5013993eead8c5afca1fe1994e6a716e7b3d1245
Fahmi Syahrul Yahya <117266406+codewithfahmi@users.noreply.github.com>
2024-11-01 03:17:49 +0000

    Duaaarrr

--- single-linked-list/src/App.java
+++ single-linked-list/src/App.java
@@ -1,42 +1,26 @@
-import java.lang.reflect.*;
+import core.algorithm.*;
+import model.Mahasiswa;
 
 public class App {
-	public static class MachineType {
-		public String machineType;
-	}
-
-	public static class Vehicle {
-		public int number;
-		public String type;
-		public MachineType machineType;
-	}
-
-	public static class Person {
-		public String name;
-		public String gender;
-		public Vehicle vehicle;
-		public String[] hobbies;
-	}
-
-	public void printAttributes(Class<?> c, int indent) {
-		Field[] fields = c.getDeclaredFields();
-		System.out.println(" ".repeat(indent) + c.getSimpleName() + " {");
-		for (Field field : fields) {
-			field.setAccessible(true);
-			String formatName = " ".repeat(indent + 1)
-					+ " " + field.getName() + ": "
-					+ field.getType().getSimpleName();
-			if (field.getType().equals(String.class) || field.getType().isPrimitive() || field.getType().isArray()) {
-				System.out.println(formatName);
-			} else {
-				this.printAttributes(field.getType(), indent + 2);
-			}
-		}
-		System.out.println(" ".repeat(indent) + "}");
-	}
-
 	public static void main(String[] args) throws Exception {
-		App app = new App();
-		app.printAttributes(Person.class, 0);
+		SingleLinkedList<Mahasiswa> mahasiswaList = new SingleLinkedList<>();
+		// Praktik - 1
+		mahasiswaList.addFirst(new Mahasiswa("Agung BP", "Jakarta", 23, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 2.3F));
+		mahasiswaList.addFirst(new Mahasiswa("Rulieta", "Kuala Kapuas", 19, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 3.3F));
+		mahasiswaList.addFirst(new Mahasiswa("Karti", "Palembang", 26, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.9F));
+		mahasiswaList.addLast(new Mahasiswa("Hermon", "Banjarmasin", 31, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1F));
+		mahasiswaList.addLast(new Mahasiswa("Anis", "Surakarta", 17, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.2F));
+		mahasiswaList.addLast(new Mahasiswa("Yanti", "Purwokerto", 19, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.5F));
+		mahasiswaList.addFirst(new Mahasiswa("Handono", "Bekasi", 20, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 4F));
+		mahasiswaList.addFirst(new Mahasiswa("Anton", "Bogor", 25, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 3.5F));
+		mahasiswaList.addFirst(new Mahasiswa("Satrio", "Yogyakarta", 19, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1.3F));
+		mahasiswaList.display();
+
+		// Pratik - 2
+		mahasiswaList.addLast(new Mahasiswa("Hermon", "Banjarmasin", 31, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1F));
+		mahasiswaList.addLast(new Mahasiswa("Anis", "Surakarta", 17, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.2F));
+		mahasiswaList.addLast(new Mahasiswa("Yanti", "Purwokerto", 19, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.5F));
+		mahasiswaList.addLast(new Mahasiswa("Satrio", "Yogyakarta", 19, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1.3F));
+		mahasiswaList.display();
 	}
 }
