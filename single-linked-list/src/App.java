80e8a9a8987972108a45bf320d0ab91dd29dd3d3
Fahmi Syahrul Yahya <117266406+codewithfahmi@users.noreply.github.com>
2024-11-11 09:47:04 +0000

    Kalau ada petir, janganlah engkau lari

--- single-linked-list/src/App.java
+++ single-linked-list/src/App.java
@@ -3,26 +3,21 @@ import model.Mahasiswa;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		System.out.println("test!");
 		SingleLinkedList<Mahasiswa> mahasiswaList = new SingleLinkedList<>();
-		// Praktik - 1
 		mahasiswaList.addFirst(new Mahasiswa("Agung BP", "Jakarta", 23, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 2.3F));
 		mahasiswaList.addFirst(new Mahasiswa("Rulieta", "Kuala Kapuas", 19, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 3.3F));
 		mahasiswaList.addFirst(new Mahasiswa("Karti", "Palembang", 26, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.9F));
-		mahasiswaList.addFirst(new Mahasiswa("Handono", "Bekasi", 20, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 4F));
-		mahasiswaList.add(new Mahasiswa("Anton", "Bogor", 25, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 3.5F), 1);
-		mahasiswaList.addLast(new Mahasiswa("Hermon", "Banjarmasin", 31, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1F));
+		mahasiswaList.addFirst(new Mahasiswa("Hermon", "Banjarmasin", 31, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1F));
+		
 		mahasiswaList.addLast(new Mahasiswa("Anis", "Surakarta", 17, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.2F));
 		mahasiswaList.addLast(new Mahasiswa("Yanti", "Purwokerto", 19, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.5F));
-		mahasiswaList.addLast(new Mahasiswa("Hermon", "Banjarmasin", 31, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1F));
+		mahasiswaList.addLast(new Mahasiswa("Handono", "Bekasi", 20, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 4F));
+		mahasiswaList.addLast(new Mahasiswa("Anton", "Bogor", 25, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 3.5F));
 		
-		mahasiswaList.display();
+		mahasiswaList.add(new Mahasiswa("Satrio", "Yogyakarta", 19, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1.3F), 5);
+		
+		mahasiswaList.remove(0);
 
-		// Pratik - 2
-		// mahasiswaList.addFirst(new Mahasiswa("Satrio", "Yogyakarta", 19, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1.3F));
-		// mahasiswaList.addLast(new Mahasiswa("Anis", "Surakarta", 17, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.2F));
-		// mahasiswaList.addLast(new Mahasiswa("Yanti", "Purwokerto", 19, 'P', new String[] {"Makan", "Tidur", "Ngoding"}, 1.5F));
-		// mahasiswaList.addLast(new Mahasiswa("Satrio", "Yogyakarta", 19, 'L', new String[] {"Makan", "Tidur", "Ngoding"}, 1.3F));
-		// mahasiswaList.display();
+		mahasiswaList.display();
 	}
 }
